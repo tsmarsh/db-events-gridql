fd74b32af3c75bc3a4dc538a219c30487988f605
Tom Marsh <ts.marsh@gmail.com>
2024-03-21 17:50:51 -0400

    Now we're as broken as we were before... progress?

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ import { start } from "@gridql/mongo-event-builder";
 import { init } from "@gridql/mongo-event-builder/lib/config.js";
 import fs from "fs";
 
+import Log4js from "log4js";
+
+
+Log4js.configure({
+    appenders: {
+        out: {
+            type: 'stdout'
+        }
+    },
+    categories: {
+        default: { appenders: ['out'], level: 'info' }
+    }
+});
+
+const logger = Log4js.getLogger("db-events")
+
 let configPath = "./config/config.conf";
 
 if (fs.existsSync(configPath)) {
   init(configPath)
     .then((config) => {
-      console.log("Configuration found: ", config);
+      logger.info(`Configuration found: ${JSON.stringify(config)}`);
 
       start(config).catch((err) => {
-        console.log("Failed to start: ", err);
+        logger.error(`Failed to start: ${JSON.stringify(err)}`);
       });
     })
-    .catch((err) => console.log("Error parsing config: ", err));
+    .catch((err) => logger.error(`Error parsing config: ${JSON.stringify(err)}`));
 } else {
-  console.log("Config missing");
+  logger.error("Config missing");
 }
